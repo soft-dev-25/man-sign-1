ca3d8980f6dab014c2ca77c8df89b3d44a1d0182
Ali Mohammad <alihmohammad@hotmail.com>
2025-10-07 14:47:38 +0200

    feat: added repositores

---
+++ backend/api/Repositories/PersonsRepository.cs
@@ -0,0 +1,11 @@
+using api.Models;
+
+namespace api.Repositories;
+
+public class PersonsRepository : IPersonsRepository
+{
+    public Postal GetPostal()
+    {
+        throw new NotImplementedException();
+    }
+}
