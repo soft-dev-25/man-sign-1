05b4fda61fa8328e8c54ea6a8eaa18d0b79f901a
Ali Mohammad <alihmohammad@hotmail.com>
2025-10-09 23:16:19 +0200

    feat: added custom BadRequestException

---
+++ backend/api/ExceptionHandlers/BadRequestException.cs
@@ -0,0 +1,12 @@
+﻿namespace api.ExceptionHandlers;
+
+public class BadRequestException : Exception
+{
+    public BadRequestException() { }
+
+    public BadRequestException(string message)
+        : base(message) { }
+
+    public BadRequestException(string message, Exception inner)
+        : base(message, inner) { }
+}
