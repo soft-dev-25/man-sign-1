a7f5d11013c095a6e0b35212117047ee9f524bfc
Shero Shvan <sheroshvan@icloud.com>
2025-10-10 17:21:14 +0200

    test: add initial NUnit test setup.

---
+++ backend/getAddressTest/UnitTest/Address.cs
@@ -0,0 +1,17 @@
+﻿namespace getAddressTest.UnitTest
+{
+    [TestFixture]
+    public class Address
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
