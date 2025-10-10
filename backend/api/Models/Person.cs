6528b7356f1e3426667712654526858333ebfc3c
Shero Shvan <sheroshvan@icloud.com>
2025-10-10 16:58:47 +0200

    refactor(address): move Address class from Person.cs to separate file

--- backend/api/Models/Person.cs
+++ backend/api/Models/Person.cs
@@ -1,18 +1,6 @@
-using System.Text.Json.Serialization;
 using api.ExceptionHandlers;
 
 namespace api.Models.DTOs;
-
-public class Address
-{
-    public string? Street { get; set; }
-    public string? Number { get; set; }
-    public int? Floor { get; set; }
-    public string? Door { get; set; }
-    public string? PostalCode { get; set; }
-    public string? TownName { get; set; }
-}
-
 public class Person
 {
     public string? Cpr { get; set; }
