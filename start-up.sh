8394632c9bb356846ea7ade5f0e0db17d4e8e7f1
Shero Shvan <sheroshvan@icloud.com>
2025-10-15 09:57:02 +0200

    Specify docker-compose file in start-up.sh for dbtest environment

--- start-up.sh
+++ start-up.sh
@@ -2,10 +2,10 @@
 # filepath: start-up.sh
 
 # Start Docker containers in detached mode
-docker-compose up -d
+docker-compose -f docker-compose.dbtest.yml up -d
 
 # Run npm tests
 npm test
 
 # docker down
-docker-compose down -v
+docker-compose -f docker-compose.dbtest.yml down -v
