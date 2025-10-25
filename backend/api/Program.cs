891441f05d1d86cfce58448029ad05270f03cb28
Ali Mohammad <113783570+AliHMohammad@users.noreply.github.com>
2025-10-25 21:47:11 +0200

    feat: added CORS (#58)
    Co-authored-by: Brezzor <oliver.bresson@gmail.com>

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -2,9 +2,28 @@ using api.DBContext;
 using api.ExceptionHandlers;
 using api.Repositories;
 using api.Services;
+using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 
+Env.TraversePath().Load();
+
 var builder = WebApplication.CreateBuilder(args);
+
+// CORS Settings
+var myCorsRule = "AllowAll";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        myCorsRule,
+        policy =>
+        {
+            policy
+            .WithOrigins(Environment.GetEnvironmentVariable("BASEURL") ?? "*")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+        }
+    );
+});
 
 // Add services to the container.
 builder.Services.AddScoped<IPersonsService, PersonsService>();
@@ -18,21 +37,10 @@ builder.Services.AddProblemDetails();
 
 builder.Services.AddControllers();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend", policy =>
-        policy.WithOrigins(Environment.GetEnvironmentVariable("BASEURL") ?? "http://localhost:8080/")
-              .AllowAnyHeader()
-              .AllowAnyMethod());
-});
-
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
-app.UseCors("AllowFrontend");
-
 
 app.UseExceptionHandler();
 
@@ -41,6 +49,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+// Apply CORS settings
+app.UseCors(myCorsRule);
 
 var httpsPort = app.Configuration["ASPNETCORE_HTTPS_PORT"];
 if (!string.IsNullOrEmpty(httpsPort))
