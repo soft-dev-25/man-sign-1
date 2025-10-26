6f9364606acb56b8fe89c26e41d03c774a31f83a
Ali Mohammad <113783570+AliHMohammad@users.noreply.github.com>
2025-10-26 19:35:13 +0100

    feat: now starts docker compose before running e2e tests (#64)
    Co-authored-by: Brian Blume <123966804+Forkeh@users.noreply.github.com>

--- scripts/endToEnd/endToEnd.sh
+++ scripts/endToEnd/endToEnd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker compose up -d # Makes sure the application is running
+
 cd ./e2e_playwrighttests/PlaywrightTests || exit 1
 
 pwsh bin/Debug/net9.0/playwright.ps1 install --with-deps
